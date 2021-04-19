f8532a0558ec86ab9abef4f810843f30387fcdf5
01x01 <thinkingandcoding@gmail.com>
2021-04-19 11:13:39 +0800

    add logger and exclude log files

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask 
-from utils.config import Config
+from common.config import Config
+import logging
+import logging.config
 
-def create_app(env):
+def create_app(filename):
     app = Flask(__name__)
-    app.config.update(Config.load(env))
+    app.config.update(Config.load(filename))
+    logging.config.dictConfig(Config.load("logger"))
 
     return app
--- common/config.py
+++ common/config.py
@@ -7,8 +7,8 @@ class Config:
         pass
 
     @staticmethod
-    def load(env):
-        with open(os.path.join("config",env + ".yaml"), "rb") as f:
+    def load(filename):
+        with open(os.path.join("config",filename + ".yaml"), "rb") as f:
             config_str = f.read()
         config_str = yaml.load(config_str)
         print(config_str)
